7be0f4ad28aa3c6ee686faf9882090789bd76b97
IagoNascimentocode <iago_ns@hotmail.com>
2021-01-30 12:31:55 -0300

    Troca de msg

---
+++ README.md
@@ -0,0 +1,5 @@
+<h1>Socket.IO</h1>
+<br>
+<h1>Sobre:</h1>
+O Socket.io é uma ferrramenta que possibilita que o back-end consiga enviar uma mensagem ou dado para o front.
+nesse primerio projeto eu faço com o socket.io que o back-end envie uma mensagem para o html e devolvo uma outra msg do html para o back-end.
--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require('express');
-const app = express();
 const path = require('path'); 
-const socketIO = require('socket.io')
+const socketIo = require('socket.io')
+const app = express();
 
 app.use('/', express.static(path.join(__dirname,"public")))
 
 const server = app.listen(8080,()=>{console.log("Server Run port:8080")})
 
-const io = socketIO(server)
+const io = socketIo(server)
 
 io.on('connection',(socket)=>{
+
     console.log("new connection")
+
+    socket.emit('hello',{msg:"Seja bem vindo ao socket.io"})
+
+    socket.on('hello_client_response',(data)=>{
+        console.log(data.msg)
+    })
 })
